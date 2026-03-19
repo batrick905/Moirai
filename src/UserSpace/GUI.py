8a31ef4d8093fe6702e52893daa6444b7d9d5057
ArturSiuda <artursiuda@debian.ArturSiuda>
2026-03-19 22:26:02 +0000

    Added NFC LOG to GUI

--- src/UserSpace/GUI.py
+++ src/UserSpace/GUI.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+import os
+
+LOG_FILE = "a"
 
 class App(tk.Tk):
 	def __init__(self):
@@ -10,7 +14,26 @@ class App(tk.Tk):
 		self.configure(bg="#ffffff")
 
 	def build_ui(self):
-		pass
+
+		#text box
+
+		tk.Label(self, text="NFC LOG", bg= "#ffffff", fg="#000000", font=("Monospace", 32)).place(x=275, y=50)
+
+		#box with nfc names
+		self.log_box = ScrolledText(self, state="disabled", bg="#111111", fg="#00ff00", font=("Monospace", 10))
+		self.log_box.place(x=150, y=100, width=450, height=150)
+		self.refresh_log()
+
+	def refresh_log(self):
+		if os.path.exists(LOG_FILE):
+			with open(LOG_FILE, "r") as f:
+				content = f.read()
+			self.log_box.configure(state="normal")
+			self.log_box.delete("1.0", "end")
+			self.log_box.insert("end", content)
+			self.log_box.configure(state = "disabled")
+			self.log_box.see("end") #scrolls to last added name
+		self.after(1000, self.refresh_log) # checks every second
 
 if __name__ == "__main__":
 	app = App()
