b4141b378aaffdf9de6d10b784bade435c0ac42b
Patrick <79091766+batrick905@users.noreply.github.com>
2026-03-19 23:40:26 +0000

    Update servo_ioctl.h

--- src/KernelSpace/Servo/servo_ioctl.h
+++ src/KernelSpace/Servo/servo_ioctl.h
@@ -1,9 +1,3 @@
-/*
- * servo_ioctl.h - Shared ioctl definitions for servo_driver
- *
- * Include this in both the kernel module AND the user-space app.
- */
-
 #ifndef SERVO_IOCTL_H
 #define SERVO_IOCTL_H
 
@@ -12,22 +6,15 @@
 #define SERVO_IOC_MAGIC 'S'
 
 struct servo_sweep_cmd {
-    int start_angle;  /* degrees [0-180] */
-    int end_angle;    /* degrees [0-180] */
-    int step;         /* step size in degrees */
-    int delay_ms;     /* milliseconds between steps */
+    int start_angle;
+    int end_angle;
+    int step;
+    int delay_ms;
 };
 
-/* Set servo to a specific angle (0-180) */
 #define SERVO_IOCTL_SET_ANGLE  _IOW(SERVO_IOC_MAGIC, 1, int)
-
-/* Get current angle */
 #define SERVO_IOCTL_GET_ANGLE  _IOR(SERVO_IOC_MAGIC, 2, int)
-
-/* Perform a sweep */
 #define SERVO_IOCTL_SWEEP      _IOW(SERVO_IOC_MAGIC, 3, struct servo_sweep_cmd)
-
-/* Centre the servo at 90 degrees */
 #define SERVO_IOCTL_CENTER     _IO (SERVO_IOC_MAGIC, 4)
 
-#endif /* SERVO_IOCTL_H */
+#endif
